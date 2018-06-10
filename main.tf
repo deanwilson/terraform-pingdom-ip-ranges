961f9c2c368cd2a47f52b79892b0c7febd74dc20
Dean Wilson <dean.wilson@gmail.com>
2018-06-10 13:23:05 +0100

    Add the basic module code
    This allows you to fetch IPv4 or 6 ranges from Pingdom

---
+++ main.tf
@@ -0,0 +1,28 @@
+/**
+* ## pingdom_ip_ranges
+*
+* A data module for retrieving Pingdoms current IPv4 or IPv6
+* address ranges.
+*
+* This module fetches the ranges from Pingdom itself and exposes them
+* as outputs.
+*
+*/
+
+locals {
+  # lowercase everything and remove whitespace
+  proto = "${lower(trimspace(var.protocol))}"
+
+  single_host_cidr = "${local.proto == "ipv4" ? "32" : "128"}"
+
+  ranges           = "${split("\n", data.http.pingdom_ranges.body)}"
+  formatted_ranges = "${formatlist("%s/%s", compact(local.ranges), local.single_host_cidr)}"
+}
+
+data "http" "pingdom_ranges" {
+  url = "https://my.pingdom.com/probes/${var.protocol}"
+
+  request_headers {
+    "Accept" = "application/json"
+  }
+}
